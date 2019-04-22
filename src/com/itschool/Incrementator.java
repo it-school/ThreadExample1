d70417c99f07ef4e625665ae52b273627fe0973e
it-school <it@osvita.od.ua>
2019-04-22 21:15:08 +0300

    Initial commit

---
+++ src/com/itschool/Incrementator.java
@@ -0,0 +1,43 @@
+package com.itschool;
+
+class Incremenator extends Thread
+{
+    //О ключевом слове volatile - чуть ниже
+    private volatile boolean mIsIncrement = true;
+    private volatile boolean mFinish = false;
+
+    public void changeAction()	//Меняет действие на противоположное
+    {
+        mIsIncrement = !mIsIncrement;
+    }
+    public void finish()		//Инициирует завершение потока
+    {
+        mFinish = true;
+    }
+
+    @Override
+    public void run()
+    {
+        do
+        {
+            if(!mFinish)	//Проверка на необходимость завершения
+            {
+                if(mIsIncrement)
+                    Main.mValue++;	//Инкремент
+                else
+                    Main.mValue--;	//Декремент
+
+                //Вывод текущего значения переменной
+                System.out.print(Main.mValue + " ");
+            }
+            else
+                return;		//Завершение потока
+
+            try{
+                Thread.sleep(100); //Приостановка потока на 1 сек.
+            }catch(InterruptedException e){}
+        }
+        while(true);
+    }
+}
+
